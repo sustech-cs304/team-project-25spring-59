19843d24f58df22168fe2028611a84fb28878b99
wtr <1977353089@qq.com>
2025-05-11 14:19:46 +0800

    初步构建在线挑战页面

--- frontier-app/src/utils/request.js
+++ frontier-app/src/utils/request.js
@@ -4,6 +4,7 @@ const request = axios.create({
 
     baseURL: 'https://m1.apifoxmock.com/m1/6156254-5848272-default'
     // baseURL: 'http://localhost:8000'
+    // baseURL: 'http://10.32.65.173'
 })
 
 export default request
