c86d5cda17c0ac979a40984a383fb64e3a620d29
Karyl01 <148410288+Karyl01@users.noreply.github.com>
2025-05-21 12:11:46 +0800

    更新任务列表的展示

--- frontier-app/src/components/TrainMission/Plans/TaskPanel.vue
+++ frontier-app/src/components/TrainMission/Plans/TaskPanel.vue
@@ -18,22 +18,32 @@ defineProps<{
 <style scoped lang="scss">
 .task-panel {
   position: absolute;
-  width: 400px;
+  width: 540px;
   height: 450px;
   overflow-y: auto;
   padding: 10px;
   border-radius: 10px;
   background: transparent;
-  backdrop-filter: blur(6px);
+  //backdrop-filter: blur(6px);
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  font-size: 14px;
+  font-size: 20px;
   color: #333;
+  //background-color: #df1c1c;
+
+  clip-path: polygon(
+    0 0,                    /* top-left */
+    calc(100% - 60px) 0,    /* move inward before top-right */
+    100% 60px,              /* cut down for right-top corner */
+    100% 100%,              /* bottom-right (keep sharp) */
+    60px 100%,              /* move inward before bottom-left */
+    0 calc(100% - 60px)     /* cut upward for left-bottom corner */
+  );
 
 }
 
 .panel-title {
   font-weight: bold;
   margin-bottom: 6px;
-  font-size: 16px;
+  font-size: 30px;
 }
 </style>
