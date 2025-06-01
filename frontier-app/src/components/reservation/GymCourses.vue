fe7619f2ac10bf679d2a586806538288185611bc
wtr <1977353089@qq.com>
2025-06-01 13:41:03 +0800

    美化排行榜，删除无用代码

--- frontier-app/src/components/reservation/GymCourses.vue
+++ frontier-app/src/components/reservation/GymCourses.vue
@@ -237,66 +237,6 @@ function handleNextClick() {
       </div>
     </div>
   </div>
-<!--  <el-row>-->
-<!--    <el-col :span="18" :offset="3">-->
-<!--      <div style="display: flex; justify-content: center">-->
-<!--        <el-image-->
-<!--            style="width: 1000px; height: 200px;"-->
-<!--            :fit="'fill'">-->
-<!--        </el-image>-->
-<!--      </div>-->
-<!--      <el-skeleton :rows="3" animated/>-->
-<!--      <div>-->
-<!--        <el-row align="middle" justify="space-between">-->
-<!--          <el-col :span="1" style="display: flex; flex-direction: row-reverse">&lt;!&ndash;left button&ndash;&gt;-->
-<!--            <el-button :icon="ArrowLeftBold" circle @click="()=>index<=0 ? index=0 : index-=5"/>-->
-<!--          </el-col>-->
-<!--          <el-col :span="4" v-for="date in dateList.data">&lt;!&ndash;dateList&ndash;&gt;-->
-<!--            <el-card shadow="hover" @click="()=>currentCourses.data = groupByDate.data[date]">-->
-<!--              {{getFullDate(new Date(date)).month}}月{{getFullDate(new Date(date)).day}}日/星期{{getFullDate(new Date(date)).week}}-->
-<!--            </el-card>-->
-<!--          </el-col>-->
-<!--          <el-col :span="1">&lt;!&ndash;right button&ndash;&gt;-->
-<!--            <el-button :icon="ArrowRightBold" circle @click="()=>index+range>=dateList.length ? index : index+=range"/>-->
-<!--          </el-col>-->
-<!--        </el-row>-->
-<!--      </div>-->
-<!--    </el-col>-->
-<!--  </el-row>-->
-<!--  <el-row>-->
-<!--    <el-col :span="16" :offset="4">-->
-<!--      <el-table :data="currentCourses.data">-->
-<!--        <el-table-column>-->
-<!--          <div class="block">-->
-<!--            <el-image-->
-<!--                style="width: 50px; height: 50px"-->
-<!--                :fit="'fill'">-->
-<!--            </el-image>-->
-<!--          </div>-->
-<!--        </el-table-column>-->
-<!--        <el-table-column label="授课教师" prop="coachName"></el-table-column>-->
-<!--        <el-table-column label="课程名称" prop="courseName"></el-table-column>-->
-<!--        <el-table-column label="课程容量" prop="capacity"></el-table-column>-->
-<!--        <el-table-column label="剩余" prop="remain"></el-table-column>-->
-<!--        <el-table-column label="上课时间" prop="startTime"></el-table-column>-->
-<!--        <el-table-column label="操作" :fixed="'right'">-->
-<!--          <template #default="scope">-->
-<!--            <el-button-->
-<!--                v-if="isReserved(scope.row.id)"-->
-<!--                type="info"-->
-<!--                disabled-->
-<!--                @click="reserveCourse(scope.row.id)"-->
-<!--            >已预定</el-button>-->
-<!--            <el-button-->
-<!--                v-else-->
-<!--                type="primary"-->
-<!--                @click="reserveCourse(scope.row.id)"-->
-<!--            >预定</el-button>-->
-<!--          </template>-->
-<!--        </el-table-column>-->
-<!--      </el-table>-->
-<!--    </el-col>-->
-<!--  </el-row>-->
 </template>
 
 <style scoped>
