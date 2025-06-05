46c7a4029f2d9586443c84629f44ed0fe11deda9
Karyl01 <148410288+Karyl01@users.noreply.github.com>
2025-06-05 10:57:43 +0800

    11

--- frontier-app/src/utils/request.js
+++ frontier-app/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseurl = 'http://10.15.81.60:8000'
+export const baseurl = 'http://10.26.63.155:8000'
 // export const baseurl = 'http://10.26.63.155:8000'  // shuyang
 // export const baseurl = 'http://10.12.184.92:8000'  // shiyansong
 const request = axios.create({
